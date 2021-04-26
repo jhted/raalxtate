df9ed512c35fc27bb7fe7d48cd7e4275a0c38dd1
Teddy <teddy@jhnet.co.za>
2021-04-26 18:11:43 +0200

    Added blogs
    added blog page and article page need to make categories for the side bar

--- raalxtate/App_Data/Models/Article.generated.cs
+++ raalxtate/App_Data/Models/Article.generated.cs
@@ -21,7 +21,7 @@ namespace Umbraco.Web.PublishedModels
 {
 	/// <summary>Article</summary>
 	[PublishedModel("article")]
-	public partial class Article : PublishedContentModel, ICarousel, IPageContent, IPageHeader, ISEO
+	public partial class Article : PublishedContentModel, ICarousel, IPageContent, IPageHeader, ISelectAnAgent, ISEO
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
@@ -71,6 +71,13 @@ namespace Umbraco.Web.PublishedModels
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "8.12.2")]
 		[ImplementPropertyType("pageTitle")]
 		public string PageTitle => global::Umbraco.Web.PublishedModels.PageHeader.GetPageTitle(this);
+
+		///<summary>
+		/// Select Agent
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "8.12.2")]
+		[ImplementPropertyType("selectAgent")]
+		public global::Umbraco.Core.Models.PublishedContent.IPublishedContent SelectAgent => global::Umbraco.Web.PublishedModels.SelectAnAgent.GetSelectAgent(this);
 
 		///<summary>
 		/// Seo Title
